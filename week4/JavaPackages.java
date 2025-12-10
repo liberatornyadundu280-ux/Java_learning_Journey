638d6ecac280d4f4f9a54f773803eec04755b9f6
liberator <liberatornyadundu280@gmail.com>
2025-12-10 21:56:33 +0530

    update

---
+++ week4/JavaPackages.java
@@ -0,0 +1,62 @@
+/*
+    A package in Java is a mechanism to group related classes, interfaces, and sub-packages into a single unit. Packages help organize large applications, avoid naming conflicts, provide access protection, and make code modular and maintainable.
+
+Avoiding name conflicts (two classes with the same name can exist in different packages)
+Providing access control using public, protected, and default access
+Reusability: packaged code can be imported and used anywhere
+Encouraging modular programming
+
+
+1. Built-in Packages
+Built-in Packages comprise a large number of classes that are part of the Java API. Some of the commonly used built-in packages are:
+
+java.lang: Contains language support classes(e.g, classes that define primitive data types, math operations). This package is automatically imported.
+java.io: Contains classes for supporting input/output operations.
+java.util: Contains utility classes that implement data structures such as Linked Lists and Dictionaries, as well as support for date and time operations.
+java.applet: Contains classes for creating Applets.
+java.awt: Contains classes for implementing the components for graphical user interfaces (like buttons, menus, etc). 6)
+
+2. User-defined Packages
+User-defined Packages are the packages that are defined by the user.
+*/
+
+
+import java.util.Random;   // built-in package
+
+public class GFG{
+    
+    public static void main(String[] args) {
+        
+        // using Random class
+        Random rand = new Random();   
+
+        // generates a number between 0–99
+        int number = rand.nextInt(100);  
+
+        System.out.println("Random number: " + number);
+    }
+}
+
+// illustrating the user defined functions
+// package com.myapp; ------> we will be stating that this class i goin to be imported in other classes
+
+public class Helper {
+    public static void show() {
+        System.out.println("Hello from Helper!");
+    }
+}
+// now using the package we we made
+// import com.myapp.Helper;----->importing our user defined package
+
+public class Test {
+    public static void main(String[] args) {
+        Helper.show();
+    }
+}
+
+public class JavaPackages {
+    public static void main(String[] args)
+    {
+        System.out.println("This code is just a hint of the java pakages i will be using in the future\n so just get familia with them ");
+    }
+}
